f3ec7f223a7f22df76b813dbdf7a2c5df1cbbecf
delusionull <jrbsly@gmail.com>
2017-07-11 07:26:09 -0500

    Update database connection to point to Dallas back end for schedule.

--- lib/db_connections.rb
+++ lib/db_connections.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Set up data connections for LWI Portal Update Daemon
+# Set up data connections
 
 require 'sequel'
 
@@ -11,7 +11,7 @@ MAS90_PATH = 'W:\\shazam\\' +
                "#{'Data Update\\' if LOCATION == 'KC'}" +
                "#{TEST == 'YES' ? 'Mas90_JB.mdb' : 'Mas90 Data Copy.mdb'}"
 SCHED_PATH = 'W:\\shazam\\' +
-             "#{TEST == 'YES' ? 'Schedule_JB.mdb' : 'Schedule Shazam.mdb'}"
+             "#{TEST == 'YES' ? 'Schedule_JB.mdb' : 'Schedule Shazam Dal_be.mdb'}"
 
 connection_string = 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source='
 DB_SCHED = Sequel.ado(:conn_string=>connection_string + SCHED_PATH)
