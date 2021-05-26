bb208db6f2a2c7ca02d990ddbe8acac055c33d3e
delusionull <jrbsly@gmail.com>
2021-05-26 17:14:03 -0400

    version 4.6.1.00 Adds a progress bar

--- lib/oscar/runner.rb
+++ lib/oscar/runner.rb
@@ -8,6 +8,7 @@ require_relative 'sales_order'
 require_relative 'schedule_so'
 require_relative 'screenprinter'
 require_relative 'csvwriter'
+require 'ruby-progressbar'
 require 'csv'
 
 class NilClass
@@ -26,12 +27,33 @@ module Oscar
 
     def run
       sos = get_so_nums
+      puts ''
+      pm = "\e[0;33m\u{15E7}"
+      blinky, pinky, inky, clyde = "\e[0;31m\u{15E9} ", "\e[0;35m\u{15E9} ", "\e[0;34m\u{15E9} ", "\e[0;33m\u{15E9} "
+      eyes = "\e[0m\u{221E} "
+      dot = "\e[0m\u{FF65}"
+      progressbar = ProgressBar.create(:format => "%a %b" + pm + dot * 6 + pinky + "\e[0m%i %p%% %t",
+                                       :progress_mark => ' ',
+                                       :remainder_mark => "\u{FF65}",
+                                       :title => "done",
+                                       :total => sos.length) unless $opts.console || $opts.nocsv
       sos.each do |so_num|
         so = SalesOrder.new(so_num)
         schedule_so = ScheduleSO.new(so)
         flgs = flags(schedule_so, so)
         Screenprinter.print(so, schedule_so, flgs) if $opts.console || $opts.nocsv
         CSVWriter.addline(so, schedule_so, flgs) unless $opts.nocsv
+        unless $opts.console || $opts.nocsv
+          dotbuff = ((progressbar.total.to_f - progressbar.progress)*10/progressbar.total).to_i - 2
+          progressbar.format =
+            "%a %b" +
+            (dotbuff == 0 ? eyes : '') +
+            (dotbuff == -1 ? eyes + ' ' : '') +
+            pm + dot * (dotbuff < 0 ? 0 : dotbuff) +
+            (dotbuff < 1 ? '' : (progressbar.progress.even? ? dot : '') + pinky) +
+            "\e[0m%i %p%% %t"
+          progressbar.increment
+        end
       end
     end
 
